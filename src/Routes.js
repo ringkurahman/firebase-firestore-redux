c25badab317df0b94e857c62491b481699564093
ringkurahman <mrringkurahman@gmail.com>
2021-02-01 09:07:39 +0400

    Created folder structure

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
+import Header from './components/Shared/Header'
 
-const Routes = () => (
-  <>
-    Hello
-  </>
-)
+import Home from './components/Home'
+
+class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/home" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
 
 export default Routes
+
